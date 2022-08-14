f15fdbce9241f1bf8aea1c8708eedbc8c23bddb2
Maurício Farah <mau.farah@hotmail.com>
2022-08-14 13:40:59 -0300

    feat: add pagination method

--- backend/src/main/java/com/devsuperio/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperio/dscatalog/resources/CategoryResource.java
@@ -2,6 +2,9 @@ package com.devsuperio.dscatalog.resources;
 
 import com.devsuperio.dscatalog.dto.CategoryDTO;
 import com.devsuperio.dscatalog.services.CategoryService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -22,6 +25,18 @@ public class CategoryResource {
     @GetMapping
     public ResponseEntity<List<CategoryDTO>> findAll() {
         List<CategoryDTO> list = categoryService.findAll();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/paged")
+    public ResponseEntity<Page<CategoryDTO>> findAllPaged(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "pageSize", defaultValue = "12") Integer pageSize,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, pageSize, Sort.Direction.valueOf(direction), orderBy);
+        Page<CategoryDTO> list = categoryService.findAllPaged(pageRequest);
         return ResponseEntity.ok(list);
     }
 
