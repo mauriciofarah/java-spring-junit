5fbc19fa6608db05f4a4f3db1c45aae4e45b0b37
Maurício Farah <mau.farah@hotmail.com>
2022-08-16 21:04:32 -0300

    feat: products delete excepetions tests

--- src/test/java/com/devsuperio/dscatalog/services/ProductServiceTests.java
+++ src/test/java/com/devsuperio/dscatalog/services/ProductServiceTests.java
@@ -1,6 +1,8 @@
 package com.devsuperio.dscatalog.services;
 
 import com.devsuperio.dscatalog.repositories.ProductRepository;
+import com.devsuperio.dscatalog.services.exceptions.DataBaseIntegrityException;
+import com.devsuperio.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,6 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -22,17 +25,24 @@ public class ProductServiceTests {
 
     private long existingId;
     private long noExistingId;
+    private long dependentId;
 
     @BeforeEach
     void setUp() throws Exception {
         existingId = 1L;
         noExistingId = 999L;
+        dependentId = 4L;
+
+        //Simulate ProductRepository methods with Mockito
 
         //Set Mockito to verify when delete service is called with existing id to do nothing;
         Mockito.doNothing().when(productRepository).deleteById(existingId);
 
         //Set Mockito to verify when delete service is called with nonexistent id to throw a EmptyResultDataAccessException;
         Mockito.doThrow(EmptyResultDataAccessException.class).when(productRepository).deleteById(noExistingId);
+
+        //Set Mockito to verify when delete service is called whit violate database integrity and throw a DataIntegrityViolationException;
+        Mockito.doThrow(DataIntegrityViolationException.class).when(productRepository).deleteById(dependentId);
     }
 
     @Test
@@ -42,5 +52,21 @@ public class ProductServiceTests {
 
         //Set Mockito to check if the deleteById method is called and how many invocations have been made (1);
         Mockito.verify(productRepository, Mockito.times(1)).deleteById(existingId);
+    }
+
+    @Test
+    public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+        Assertions.assertThrows(ResourceNotFoundException.class,
+                () -> productService.deleteProduct(noExistingId));
+
+        Mockito.verify(productRepository, Mockito.times(1)).deleteById(noExistingId);
+    }
+
+    @Test
+    public void deleteShouldThrowDataBaseIntegrityExceptionWhenIdDependentId() {
+        Assertions.assertThrows(DataBaseIntegrityException.class,
+                () -> productService.deleteProduct(dependentId));
+
+        Mockito.verify(productRepository, Mockito.times(1)).deleteById(dependentId);
     }
 }
